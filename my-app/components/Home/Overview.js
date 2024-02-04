77b9fd8344e6619e90faa792860ac05dcfae54d1
dennissammy77 <dennissammy77@gmail.com>
2024-02-04 22:18:18 +0300

    Wallet card design at overview component

--- my-app/components/Home/Overview.js
+++ my-app/components/Home/Overview.js
@@ -1,13 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
 export default function Overview() {
   return (
-    <View style={{display:'flex',flex:1}}>
-      <View style={{borderRadius:20,backgroundImage:'../../assets/Designs/wallet_card.jpg',height:'200px'}}>
+    <View style={{display:'flex',flex:1,alignItems:'center',marginVertical:25}}>
+      <View style={{borderRadius:30,padding:30,height:200,width:'100%',zIndex:0,backgroundColor:'#292c31'}}>
         <Text>Overview</Text> 
       </View>
-      <Text>Overview</Text>
+      <View style={{borderRadius:30,padding:30,height:200,width:'95%',backgroundColor:'#836ee7',marginTop:-208,zIndex:-10}}>
+        <Text>Overview</Text> 
+      </View>
+      <View style={{borderRadius:30,padding:30,height:200,width:'85%',backgroundColor:'#Cac3ee',marginTop:-210,zIndex:-20}}>
+        <Text>Overview</Text> 
+      </View>
     </View>
   )
 }
