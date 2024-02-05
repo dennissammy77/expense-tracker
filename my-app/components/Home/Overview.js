b24fd2c9634d64604ba6b1150741fc4c4961a895
dennissammy77 <dennissammy77@gmail.com>
2024-02-05 23:53:53 +0300

    Wallet card design at overview component added an image and details

--- my-app/components/Home/Overview.js
+++ my-app/components/Home/Overview.js
@@ -1,11 +1,42 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { COLORS, SIZES } from '../../constants';
+import { Image } from 'expo-image';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Overview() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Poppins-Bold': require('../../assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <View style={{display:'flex',flex:1,alignItems:'center',marginVertical:25}}>
-      <View style={{borderRadius:30,padding:30,height:200,width:'100%',zIndex:0,backgroundColor:'#292c31'}}>
-        <Text>Overview</Text> 
+    <View style={{display:'flex',flex:1,alignItems:'center',marginVertical:25}} onLayout={onLayoutRootView}>
+      <View style={{borderRadius:30,padding:30,height:200,width:'100%',zIndex:0,backgroundColor:'#292c31',display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
+        <Text style={{fontSize:SIZES.xxLarge,fontWeight:'bold',color:COLORS.white,fontFamily:'Poppins-Bold'}}>Wallet</Text>
+        <View style={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+          <View>
+            <Text style={{fontSize:SIZES.medium,fontWeight:'300',color:COLORS.white,marginVertical:10}}>Total balance</Text>
+            <Text style={{fontSize:SIZES.xLarge,fontWeight:'bold',color:COLORS.white,fontFamily:'Poppins-Bold'}}>Ksh 1,000,000</Text>
+          </View>
+          <Image
+            source={require('../../assets/images/coin_img.png')}
+            contentFit="cover"
+            transition={1000}
+            style={{width:100,height:100}}
+          />
+        </View>
       </View>
       <View style={{borderRadius:30,padding:30,height:200,width:'95%',backgroundColor:'#836ee7',marginTop:-208,zIndex:-10}}>
         <Text>Overview</Text> 
--- my-app/constants/images.js
+++ my-app/constants/images.js
@@ -1,9 +1,12 @@
 import intro_img from '../assets/images/intro.jpg';
 import intro2 from '../assets/images/intro2.jpg';
 import home_bg from '../assets/images/home_bg.jpg';
+import coin_img from '../assets/images/coin_img.png';
+
 
 export default {
     intro_img,
     intro2,
-    home_bg
+    home_bg,
+    coin_img
 }
